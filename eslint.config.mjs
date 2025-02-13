2745f0882e7cc6fa313939a9362c58d1c22004e6
NishideKenji <132211570+NishideKenji@users.noreply.github.com>
2025-02-13 22:28:48 +0900

    拡張機能の有効化、ESLintとPrettierの設定
    ①パッケージインストール
    npm install @typescript-eslint/eslint-plugin@^8.22.0 eslint-config-prettier@^10.0.1 eslint-plugin-simple-import-sort@^12.1.1 prettier npm-run-all@^4.1.5
    
    ②設定追加
    
    拡張機能の有効化、ESLintとPrettierの設定②設定追加
    ・eslint.config.mjsを追加・設定
    ・package.jsonのscriptsにealintの設定などを追記
    ・.prettierignoreを追加・設定
    ・.prettierrcを追加・設定
    ・.vscode\setting.jsonを追加
    
    ●VS Codeへの変更
    ・VS CodeのES LintとPrettier拡張を導入
    
    ③コード成型
    各ファイルを触って、右クリック→Quick Fix→修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'simple-import-sort': simpleImportSort,
+      import: importPlugin,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
