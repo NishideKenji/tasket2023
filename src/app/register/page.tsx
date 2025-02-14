3c335d7850bb9b6393d553f8476594d3ffc4412c
NishideKenji <132211570+NishideKenji@users.noreply.github.com>
2025-02-14 17:55:56 +0900

    ユーザー管理機能の追加①-2ユーザー登録画面に検証機能の追加
    ユーザー登録画面に基本的な検証を入れる
    (サーバー側への伝達はまだ)
    
    > npm install @hookform/resolvers@^3.10.0

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,28 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Box, Button, Container, TextField } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const taskCreateSchema = z.object({
+  username: z.string().nonempty(),
+  email: z.string().nonempty().email(),
+  password: z.string().nonempty(),
+})
 
 export default function RegisterForm() {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState } = useForm({
     defaultValues: {
       username: '',
       email: '',
       password: '',
     },
+    resolver: zodResolver(taskCreateSchema),
+    mode: 'onTouched',
   })
+
   return (
     <Container maxWidth="xs">
       <Box>
@@ -21,28 +32,60 @@ export default function RegisterForm() {
           })}
         >
           <h3>Register</h3>
+
           <TextField
             {...register('username')}
             fullWidth
             margin={'normal'}
             label="User Name"
             type="text"
+            error={
+              formState.touchedFields.username &&
+              Boolean(formState.errors.username)
+            }
+            helperText={
+              formState.touchedFields.username &&
+              formState.errors?.username?.message
+            }
           />
+
           <TextField
             {...register('email')}
             fullWidth
             margin={'normal'}
             label="Email"
             type="email"
+            error={
+              formState.touchedFields.email && Boolean(formState.errors.email)
+            }
+            helperText={
+              formState.touchedFields.email && formState.errors?.email?.message
+            }
           />
+
           <TextField
             {...register('password')}
             fullWidth
             margin={'normal'}
             label="password"
             type="password"
+            error={
+              formState.touchedFields.password &&
+              Boolean(formState.errors.password)
+            }
+            helperText={
+              formState.touchedFields.password &&
+              formState.errors?.password?.message
+            }
           />
-          <Button type="submit" variant="contained">
+
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={
+              formState.isSubmitting || !formState.isDirty || !formState.isValid
+            }
+          >
             Submit
           </Button>
         </form>
