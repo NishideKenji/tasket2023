cabc4ed83b7a4685b5fb4dbba126be9415371478
NishideKenji <132211570+NishideKenji@users.noreply.github.com>
2025-02-15 14:14:17 +0900

    タスク管理機能の追加②タスク一覧表示機能の追加
    一覧表示用コンポーネント・API作成
    一覧表示機能をトップページに追加

---
+++ src/app/_components/task/tasklist.tsx
@@ -0,0 +1,52 @@
+import {
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material'
+import { type Task } from '@prisma/client'
+import dayjs from 'dayjs'
+
+interface Props {
+  tasks: Task[]
+}
+
+export const TaskList = ({ tasks }: Props) => {
+  return (
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell>No</TableCell>
+          <TableCell>タイトル</TableCell>
+          <TableCell>完了状況</TableCell>
+          <TableCell>計画日時</TableCell>
+          <TableCell>完了日時</TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        {tasks.map((task, index) => {
+          return (
+            <TableRow key={task.id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{task.title}</TableCell>
+              <TableCell>
+                <Checkbox disabled={true} checked={task.is_finish} />
+              </TableCell>
+              <TableCell>
+                {task.end_date_scheduled &&
+                  dayjs(task.end_date_scheduled).format('YYYY/MM/DD')}
+              </TableCell>
+              <TableCell>
+                {task.end_date_actual &&
+                  dayjs(task.end_date_actual).format('YYYY/MM/DD')}
+              </TableCell>
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,51 @@
 'use client'
 
-import { Box, Button, Container, Typography } from '@mui/material'
+import { Button, Container, Grid2, Typography } from '@mui/material'
 import { signIn, useSession } from 'next-auth/react'
 
+import { trpc } from '@/trpc/client'
+
 import { TaskDetails } from './_components/task/taskdetails'
+import { TaskList } from './_components/task/tasklist'
 
 export default function Home() {
   const { data: session } = useSession()
 
+  const { data: tasks } = trpc.taskRouter.list.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+  })
+
   return (
-    <Container maxWidth="md">
-      <Box textAlign="center" py={10}>
-        {session ? (
-          <>
-            <Typography variant="h5" gutterBottom>
-              Create New Task
-            </Typography>
-            <TaskDetails
-              task={{
-                id: '',
-                title: '',
-                is_finish: false,
-                description: '',
-                end_date_scheduled: null,
-                end_date_actual: null,
-              }}
-            />
-          </>
-        ) : (
-          <Button onClick={() => signIn()}>signIn</Button>
-        )}
-      </Box>
+    <Container maxWidth={false}>
+      {session ? (
+        <>
+          <Grid2 container spacing={1} sx={{ p: 1, mt: 3 }}>
+            <Grid2 size={6} sx={{ p: 2 }}>
+              <Typography variant="h5" gutterBottom>
+                Task List
+              </Typography>
+              <TaskList tasks={tasks || []} />
+            </Grid2>
+            <Grid2 size={6} sx={{ p: 2, borderLeft: '1px solid #ccc' }}>
+              <Typography variant="h5" gutterBottom>
+                Create New Task
+              </Typography>
+              <TaskDetails
+                task={{
+                  id: '',
+                  title: '',
+                  is_finish: false,
+                  description: '',
+                  end_date_scheduled: null,
+                  end_date_actual: null,
+                }}
+              />
+            </Grid2>
+          </Grid2>
+        </>
+      ) : (
+        <Button onClick={() => signIn()}>signIn</Button>
+      )}
     </Container>
   )
 }
